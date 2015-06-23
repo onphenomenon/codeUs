c1391e5183eda0721419f2222d22a7cb1750c294
Kari Burns <kariannechristina@gmail.com>
2015-06-23 16:34:17 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+require('server/config/middleware.js')(app, express);
+
+module.exports = app;
+
+app.listen(8000);
+
+var port = 8000;
+var ip = "127.0.0.1";
+console.log("Listening on http://" + ip + ":" + port);
+
